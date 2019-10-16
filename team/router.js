2a1163baab23296879cdb0d6c2610448b32366f5
marloesverdonk <marloesverdonk@live.nl>
2019-10-16 18:13:12 +0200

    Search for an endpoint id

--- team/router.js
+++ team/router.js
@@ -3,16 +3,22 @@ const Team = require('./model')
 
 const teamRouter = new Router
 
-teamRouter.get('/team', (req, res, next) => 
+teamRouter.get('/teams', (req, res, next) => {
   Team.findAll()
       .then(teamlist => res.send(teamlist))
       .catch(next)
-)
+})
 
-teamRouter.post('/team', (req, res, next) => 
+teamRouter.post('/teams', (req, res, next) => {
   Team.create(req.body)
       .then(team => res.send(team))
       .catch(next)
-)
+})
+
+teamRouter.get('/teams/:id', (req, res, next) => {
+  Team.findByPk(req.params.id)
+      .then(team => res.send(team))
+      .catch(next)
+})
 
 module.exports = teamRouter
